cc2b185fe9a2a425a6c047362342a74f0a347c25
jizrix <jizrix@gmail.com>
2023-09-14 01:42:28 +0700

    add simple validate and alert

--- src/Form.jsx
+++ src/Form.jsx
@@ -7,21 +7,28 @@ const Form = ({createData}) => {
     const [position,setPosition] = useState('');
 
     const buttonHandler = () => {
-        const  uid = uuidv4();   
-        const data= {
+        if(name !== "" && lastname !== "" && position !== ""){
+            const  uid = uuidv4();   
+            const data= {
             id:uid,
             name:name,
             lastname:lastname,
             position:position
         }
         createData(data)
+        setName("")
+        setLastname("")
+        setPosition("")
+        }else{
+            alert("Bad input")
+        }
     }
 
     return(
         <div className="create-user">
-            <input onChange={(e)=>setName(e.target.value)} type="text" placeholder="name"/>
-            <input onChange={(e)=>setLastname(e.target.value)} type="text" placeholder="lastname"/>
-            <input onChange={(e)=>setPosition(e.target.value)} type="text" placeholder="position"/>
+            <input onChange={(e)=>setName(e.target.value)} type="text" placeholder="name" value={name}/>
+            <input onChange={(e)=>setLastname(e.target.value)} type="text" placeholder="lastname" value={lastname}/>
+            <input onChange={(e)=>setPosition(e.target.value)} type="text" placeholder="position" value={position}/>
             <button onClick={() => buttonHandler()}>Save</button>
         </div>
     )
